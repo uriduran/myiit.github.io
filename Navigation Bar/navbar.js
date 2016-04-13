dc209c9cad12ffbb697e028da2fb7f3af606c104
Uriel Duran <uduran12@gmail.com>
2016-04-13 15:24:41 -0500

    Nav bar updated

---
+++ Navigation Bar/navbar.js
@@ -0,0 +1,42 @@
+var closedmen = '<img id= "threebar" alt="Menu" src="http://i.imgur.com/kAXCJ8s.png">'
+var openmen = '<img id= "threebarc" alt="Menu" src="http://i.imgur.com/ex7Hcuq.png?2">'
+
+
+$(document).ready(function(){
+		$count=1;
+		$('#menulink').click(function(){
+			if($count%2!=0)
+			{
+			$('#menu').animate({"marginLeft":"0px"},500,function(){
+				$('#menulink').html(openmen);
+			});
+			
+			$count++;
+			}
+			else
+			{
+			$('#menu').animate({"marginLeft":"-200px"},500,function(){
+			$('#menulink').html(closedmen);
+			});
+			
+			$count++;
+			}
+		});
+  
+  $('#menu li:has("ul")').children('ul').hide(); //hide submenu
+$('#button').toggle(
+function () {
+    $('#right').animate({
+        left: 150
+    });
+},
+function () {
+    $('#right').animate({
+        left: 0
+    });
+});
+$('#menu li:has("ul")').click(function(){ 
+    $(this).children('ul').slideToggle(); //toggle submenu
+});
+
+});

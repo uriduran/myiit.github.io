2641f454531f646ce38439f61f2d100f86c103f9
Uriel Duran <uduran12@gmail.com>
2016-04-16 19:41:23 -0500

    Tons of comments to my js

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,28 @@
+
+//Creating variables with the two different images to insert when clicked.
 var closedmen = '<img id= "threebar" alt="Menu" src="http://i.imgur.com/kAXCJ8s.png">'
 var openmen = '<img id= "threebarc" alt="Menu" src="http://i.imgur.com/ex7Hcuq.png?2">'
-
-
 //Function to change the top menu icon to show whether icon is open or closed.
 $(document).ready(function(){
 		$count=1;
 		$('#menulink').click(function(){
 			if($count%2!=0)
 			{
-			$('#menu').animate({"marginLeft":"0px"},300,function(){
-				$('#menulink').html(openmen);
+			$('#menu').animate({"marginLeft":"0px"},300,function(){ //opens up the side bar menu
+				$('#menulink').html(openmen); //injects html with image to indicate menu opened.
 			});
-			
 			$count++;
 			}
 			else
 			{
-			$('#menu').animate({"marginLeft":"-200px"},300,function(){
-			$('#menulink').html(closedmen);
+			$('#menu').animate({"marginLeft":"-200px"},300,function(){ //closes the side bar menu
+			$('#menulink').html(closedmen); //injects html with image to indicate menu closed
 			});
 			
 			$count++;
 			}
 		});
-        
-       
-        
-   
-  $('#menu li:has("ul")').children('ul').hide(); //hide submenu
+$('#menu li:has("ul")').children('ul').hide(); //Hide submenu when a submenu item is clicked
 $('#button').toggle(
 function () {
     $('#right').animate({
